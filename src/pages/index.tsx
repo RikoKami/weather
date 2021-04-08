c8f57b4da1018c84f8472fd755ad9f905d966171
Riko Kami <orglary@gmail.com>
2021-04-07 21:26:58 -0300

    feat: initial head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Weather</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
