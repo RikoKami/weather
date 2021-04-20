338ebe4702a566acaf71c360b6fef2a52804a67b
Riko Kami <orglary@gmail.com>
2021-04-20 19:50:50 -0300

    style: add file for utils in css

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -7,7 +7,6 @@ export const Aside = () => {
   const getLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition)
-      console
     } else {
       console.log('Geolocation is not supported by this browser.')
     }
