3810cce453a8870f8115793c6d5bc40ebd7717a0
Riko <orglary@gmail.com>
2021-05-02 23:52:33 -0300

    feat: initial react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
+import React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient()
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
