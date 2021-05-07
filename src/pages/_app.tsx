702ff8d3830588766007e5ea3dde1283663a47c0
Riko <orglary@gmail.com>
2021-05-06 22:56:58 -0300

    fix: remove react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,8 @@
 import React from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { Hydrate } from 'react-query/hydration'
 import '../styles/globals.scss'
 
 const MyApp = ({ Component, pageProps }) => {
-  const queryClient = new QueryClient()
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-      </Hydrate>
-    </QueryClientProvider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
